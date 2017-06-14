b5950948cc3261ffa19abe497c1eb025047c8a14
fclarat <fclarat@hotmail.com>
2017-06-14 19:17:29 -0300

    modelos y migrations

---
+++ database/migrations/2017_06_14_185014_create_equipos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEquiposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('jugador_id')->references('id')
+                ->on('jugadors')->onDelete('cascade');
+            $table->foreign('partido_id')->references('id')
+                ->on('partidos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipos');
+    }
+}
