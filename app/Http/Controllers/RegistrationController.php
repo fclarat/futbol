8c342edd6cfdabb7a9e864cf8bcfbb45389af934
Nahuel Lattessi <n_lattessi@hotmail.com>
2017-06-16 22:23:44 -0300

    login and register wip

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class RegistrationController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        // Validate the form
+        $this->validate(request(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // Creae and save the user
+        $user = User::create(request(['name', 'email', 'password']));
+
+        // Sign them in
+        auth()->login($user);
+
+        // Redirect to the home
+        return redirect()->home();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/jugadores', 'JugadoresController@index');
+
+Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
+
+Route::get('/login', 'SessionsController@create');
+Route::get('/logout', 'SessionsController@destroy');
